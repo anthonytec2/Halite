f1ea75aaa00d608b1e31ce602e6107a1424903c2
Anthony Bisulco <bisulco.a@husky.neu.edu>
2018-12-30 16:30:05 -0500

    WIP Gym Env with networking

--- algos/gym-env/gym_halite/envs/halite_env.py
+++ algos/gym-env/gym_halite/envs/halite_env.py
@@ -3,24 +3,71 @@ from gym import error, spaces, utils
 from gym.utils import seeding
 import hlt
 import logging
+import subprocess
+import psutil
+import socket
+import sys
+import numpy as np
+HOST = '127.0.0.1'
+PORT = 0
+
+import time
 
 
 class HaliteEnv(gym.Env):
     metadata = {'render.modes': ['human']}
 
-    def __init__(self):
-        self.game = hlt.Game()
-        self.game.ready("MyPythonBot2")
-        logging.info(
-            "Successfully created bot! My Player ID is {}.".format(self.game.my_id))
-
     def step(self, action):
-        self.game.end_turn(action)
-        self.game.update_frame()
-        return self.game
-
-    def reset(self):
-        sys.exit(0)
+        act_msg = 'A '+str(action)
+        self.conn.send(act_msg.encode("utf-8"))
+        data = self.conn.recv(4096)
+        res = np.frombuffer(data)
+        obs = res[:27]
+        mask = res[27:34]
+        reward = res[-2]
+        done = res[-1]
+        done = True if done == 1 else False
+        obs = {
+            "action_mask": mask,
+            "real_obs": obs,
+        }
+        return obs, reward, done, {}
 
     def render(self, mode='human', close=False):
-        sys.exit(0)
+        pass
+
+    def reset(self):
+        self.establish_conn()
+        data = self.conn.recv(4096)
+        return np.frombuffer(data)
+
+    def establish_conn(self):
+        try:
+            self.s.close()
+            del self.s, self.conn, self.addr
+        except:
+            o = 2
+        try:
+            self.res.kill()
+        except:
+            o = 1
+
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.s.bind((HOST, PORT))
+        print(f'Bound Localhost Port: {self.s.getsockname()[1]}')
+        cmd = f'./halite --replay-directory replays/ -vvv --width 32 --height 32 "python3 bots/networking.py --port={self.s.getsockname()[1]}" "python3 bots/Bot2.py" --no-timeout'
+        self.res = psutil.Popen(cmd, shell=True, stdout=subprocess.PIPE)
+        self.s.listen()
+        self.conn, self.addr = self.s.accept()
+
+
+if __name__ == '__main__':
+    new_env = HaliteEnv()
+    new_env.reset()
+    new_env.step(1)
+    time.sleep(0.3)
+    new_env.step(1)
+    time.sleep(0.3)
+    new_env.step(1)
+    time.sleep(0.3)
+    new_env.s.close()
---
+++ bots/networking.py
@@ -0,0 +1,122 @@
+import argparse
+import sys
+import socket
+import logging
+import argparse
+from ray.rllib.utils.policy_client import PolicyClient
+import hlt
+from hlt import constants
+from hlt.positionals import Direction
+from hlt.positionals import Position
+import numpy as np
+import torch
+from sklearn.decomposition import TruncatedSVD
+import os
+import psutil
+
+
+def cleanup():
+    for ship in me.get_ships():
+        halite_ship = ship.halite_amount
+    reward = (game.me.halite_amount+.01*halite_ship)-reward_base
+    reward_base = reward
+    obs = get_obs(game, tf)
+    valid_mask = is_valid_move(game)
+    new_ar = np.hstack((obs, valid_mask))
+    send_obs = np.append(new_ar, [reward, 1])
+    s.send(send_obs.tostring())
+
+    game.end_turn(command_queue)
+    sys.exit(0)
+
+
+def get_func(ship, action, me):
+    if action == 0:
+        return ship.move('n')
+    elif action == 1:
+        return ship.move('e')
+    elif action == 2:
+        return ship.move('s')
+    elif action == 3:
+        return ship.move('w')
+    elif action == 4:
+        return ship.stay_still()
+    elif action == 5:
+        return ship.make_dropoff()
+    elif action == 6:
+        return me.shipyard.spawn()
+
+
+def get_obs(game, tf):
+    board_distro = np.zeros((game.game_map.height, game.game_map.width))
+    for i in range(0, game.game_map.height):
+        for j in range(0, game.game_map.width):
+            board_distro[i][j] = game.game_map[Position(i, j)].halite_amount
+    bd = tf.transform(np.reshape(board_distro/1000, (1, 1024)))
+    logging.info(game.me.get_ships())
+    for ship in game.me.get_ships():
+        posagentX = ship.position.x
+        posagentY = ship.position.y
+        shipHal = ship.halite_amount/1000
+    posdpX = game.me.shipyard.position.x
+    posdpY = game.me.shipyard.position.y
+    halite = game.me.halite_amount/1e5
+    turn_num = game.turn_number/constants.MAX_TURNS
+    return np.append(bd, [posagentX, posagentY, shipHal, posdpX, posdpY, halite, turn_num])
+
+
+def is_valid_move(game):
+    action_mask = np.ones(7)
+    # if game.me.halite_amount < 1000:
+    action_mask[6] = 0
+    # if game.me.halite_amount < 4000:
+    action_mask[5] = 0
+
+    logging.info(me.get_ships())
+    for ship in me.get_ships():
+        hal_cell = game.game_map[ship.position].halite_amount
+        logging.info('Halite Cell: '+str(hal_cell) +
+                     ' Halite Ship: ' + str(ship.halite_amount))
+        if ship.halite_amount < hal_cell:
+            action_mask[0:4] = 0
+    logging.info(action_mask)
+    return action_mask
+
+
+HOST = '127.0.0.1'
+parser = argparse.ArgumentParser()
+parser.add_argument("--port", help="Ports IP", default=9900, type=int)
+args = parser.parse_args()
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, args.port))
+    game = hlt.Game()
+    tf = torch.load(os.path.join(os.getcwd(), 'bots/encoder.tf'))
+    game.ready("Ray-BOT-Networking-V3")
+    game.update_frame()
+    game.end_turn([game.me.shipyard.spawn()])
+    reward_base = game.me.halite_amount
+    game.update_frame()
+    obs = get_obs(game, tf)
+    s.send(obs.tostring())
+    while True:
+        data = s.recv(24).decode()
+        action = int(data.split(' ')[1])
+        me = game.me
+        game_map = game.game_map
+        command_queue = []
+        for ship in me.get_ships():
+            command_queue.append(get_func(ship, action, me))
+        if game.turn_number == constants.MAX_TURNS:
+            cleanup()
+        game.end_turn(command_queue)
+        game.update_frame()
+        for ship in me.get_ships():
+            halite_ship = ship.halite_amount
+        reward = (game.me.halite_amount+.01*halite_ship)-reward_base
+        reward_base = reward
+        obs = get_obs(game, tf)
+        valid_mask = is_valid_move(game)
+        new_ar = np.hstack((obs, valid_mask))
+        send_obs = np.append(new_ar, [reward, 0])
+        s.send(send_obs.tostring())
